024729eb23984fa1e61d2ed9e895080d54ca3ec8
Bharat Choudhary <ROHANCHOUDHARY0110B@GMAIL.COM>
2025-10-05 03:37:30 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+
+void solve() {
+    // logic
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t; // Uncomment if multiple test cases
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
